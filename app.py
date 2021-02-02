3c1a76ae2147ec98c6b7fa4c4d1a8495b8e10e89
Pretro <jort2212@outlook.com>
2021-02-02 18:14:00 +0000

    Add task collapsible setup

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def register():
         #We call the users collection on MongoDB and use the insert method
         mongo.db.users.insert_one(register)
 
-        # Put the new user into 'session' cookie (look at the begining of the code)
+        # Put the new user into 'session' cookie 
         session["user"] = request.form.get("username").lower()
         flash("Registration Successfull")
         return redirect(url_for("profile", username=session["user"]))
@@ -85,7 +85,19 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+    
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
